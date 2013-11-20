1a14b8690fab4df40a2a112b1450228f2c777066
Adrian Lopez <adrianlzt@gmail.com>
2013-11-20 21:25:22 +0100

    Obtain input/output bytes written by pid

--- processes.py
+++ processes.py
@@ -1,8 +1,12 @@
 import collectd
-import random
+import os,re
 
 # global variables
 
+
+# True if dir is only numbers
+def ispid(dir):
+  return re.match('^[0-9]*$',dir) != None
 
 def config_processes(conf):
   collectd.info('processes: configuring...')
@@ -11,10 +15,31 @@ def init_processes():
   collectd.info('processes: initing...')
 
 def read_processes(data=None):
-  collectd.info("processes: reading data...")
-  vl = collectd.Values(type='gauge')
-  vl.plugin='processes'
-  vl.dispatch(values=[random.random() * 100])
+  procdir = os.listdir("/proc")
+  piddir = filter(ispid, procdir)
+
+  for dir in piddir:
+    comm = file("/proc/"+dir+"/comm").read().rstrip()
+
+    # IO
+    io = file("/proc/"+dir+"/io").read()
+    read_bytes = re.search('(?<=read_bytes: ).*',io).group(0)
+    write_bytes = re.search('(?<=write_bytes: ).*',io).group(0)
+    vl = collectd.Values(type='io_octets')
+    vl.dispatch(plugin="procv2",plugin_instance=dir, values=[read_bytes,write_bytes])
+
+    # Time
+
+    # Threads
+
+    # Page faults
+
+    # Memory (VSZ,RSS) percentage?
+
+  # Aggregated data by comm?
+
+
+    break
 
 
 collectd.register_config(config_processes)
